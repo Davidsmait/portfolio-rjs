7191a23faa5752a5b9e2965eb08f4d68d750ad00
Davidsmait <deivod_halo@hotmail.com>
2020-02-14 03:24:28 -0600

    Updates

---
+++ precache-manifest.bde51d22cc44ef9cab2fc61513942d14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f371fa0e35120b2836ec3769b105a666",
+    "url": "/portfolio-rjs/index.html"
+  },
+  {
+    "revision": "1199f4a3b95be061cd29",
+    "url": "/portfolio-rjs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b2c4ef7b3c852a08bf2a",
+    "url": "/portfolio-rjs/static/js/2.0c352cbc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio-rjs/static/js/2.0c352cbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1199f4a3b95be061cd29",
+    "url": "/portfolio-rjs/static/js/main.161f0cd1.chunk.js"
+  },
+  {
+    "revision": "f7d0355f96c198ff01b1",
+    "url": "/portfolio-rjs/static/js/runtime-main.07b9939a.js"
+  }
+]);
